c2281b022ce01671ba2ceda10f4b4c7018304af9
Ivan <ragna.blood82@gmail.com>
2023-10-03 15:34:11 +0300

    continue

--- python.py
+++ python.py
@@ -1,3 +1,5 @@
+                                                     #  Данные питомца
+
 view = str(input("Введите вид питомца: "))
 name = str(input("Его кличку: "))
 age = int(input("Его возраст: "))
@@ -5,3 +7,19 @@ age = int(input("Его возраст: "))
 print("У вас:", view)
 print("По кличке:", name)
 print("В возрасте:", age, "лет")
+
+print()
+                                                    # Развитие человека
+print("Этапы развития человека: ")
+print("Dryopithecus ", " weAustralopithecus ", " Homo erectus ", " Homo sapiens ",sep="=>")
+
+print()
+
+print("Назовите этапы развития человека: ")
+
+first_stage = str(input("Первая стадия: "))
+second_stage = str(input("Вторая стадия: "))
+third_stage = str(input("Третья стадия: "))
+fourth_stage = str(input("Четвертая стадия: "))
+
+print(first_stage , second_stage , third_stage , fourth_stage , sep=" => ")
