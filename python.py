e3b4353f987fbcd132b303908b3c721a63e5ca17
Ivan <ragna.blood82@gmail.com>
2023-10-03 14:54:53 +0300

    begin

---
+++ python.py
@@ -0,0 +1,7 @@
+view = str(input("Введите вид питомца: "))
+name = str(input("Его кличку: "))
+age = int(input("Его возраст: "))
+
+print("У вас:", view)
+print("По кличке:", name)
+print("В возрасте:", age, "лет")
